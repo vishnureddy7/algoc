0b84a79bb96690bdb630d18cbdf362c8b6bcd0e5
U-SONUSNETWORKS\greddy <greddy@greddy.sonusnet.com>
2018-12-18 15:47:24 +0530

    Linear Search

---
+++ Searching/linear_search.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+	int n;
+	printf("\t\t\tLINEAR SEARCH\n");
+	printf("\t\t\t=============\n\n");
+	printf("Enter the size of the array: ");
+	scanf("%d",&n);
+	int a[n];
+	printf("Enter the elements of the array: ");
+	for(int i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	int k,i;
+	printf("Enter the element to be searched: ");
+	scanf("%d",&k);
+	for(i=0;i<n;i++){
+		if(a[i]==k){
+			printf("Element is found at index: %d\n\n",i);
+			break;
+		}
+	}
+	if(i==n){
+		printf("Element not found in the array\n\n");
+	}
+	return 0;
+}
